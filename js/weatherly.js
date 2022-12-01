1ebeb6ce6fc870529a4fbd708b105ad87bc282f0
Matthew <gonzalez.matthew.steven@gmail.com>
2022-12-01 14:59:19 -0500

    added ajax request for weather service.

--- js/weatherly.js
+++ js/weatherly.js
@@ -1,7 +1,16 @@
-// mapboxgl.accessToken = mapKey;
-// var map = new mapboxgl.Map({
-//     container: 'map',
-//     style: 'mapbox://styles/mapbox/streets-v9',
-//     zoom: 15,
-//     center: [-87.96028, 42.30528]
-// });
+mapboxgl.accessToken = mapKey;
+let map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v9',
+    zoom: 15,
+    center: [-87.96028, 42.30528]
+});
+
+
+$.get("http://api.openweathermap.org/data/2.5/weather", {
+    APPID: weatherKey,
+    q:     "Virginia Beach, US",
+    units: "imperial"
+}).done(function(data) {
+    console.log(data);
+});
