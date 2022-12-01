5f0a537a1fb8b8636e6db4fe5fafd815f6469d86
Matthew <gonzalez.matthew.steven@gmail.com>
2022-12-01 12:46:24 -0500

    begun building skeleton

---
+++ js/weatherly.js
@@ -0,0 +1,7 @@
+// mapboxgl.accessToken = mapKey;
+// var map = new mapboxgl.Map({
+//     container: 'map',
+//     style: 'mapbox://styles/mapbox/streets-v9',
+//     zoom: 15,
+//     center: [-87.96028, 42.30528]
+// });
